5af31c5a900ba9faeafc080bbcaf6ad2e7f91e37
Krushil1820 <krushilsheladiya72@gmail.com>
2025-05-06 06:35:14 -0400

    Fix routing for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -231,7 +231,7 @@ function App() {
         {isDarkMode ? <FaSun color="yellow" /> : <FaMoon color="black" />}
       </IconButton>
       </Tooltip>
-      <Router>
+      <Router basename="/nblk-stability-calculator">
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/evaluate" element={<EvaluationPage />} />
